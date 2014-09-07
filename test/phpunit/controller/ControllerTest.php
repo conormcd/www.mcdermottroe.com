7959e1c5f8ee163c271530f5e04d5246ffa42f84
Conor McDermottroe <conor@mcdermottroe.com>
2014-09-07 08:58:22 +0100

    Improve test coverage over controllers.

--- test/phpunit/controller/ControllerTest.php
+++ test/phpunit/controller/ControllerTest.php
@@ -41,6 +41,77 @@ extends ControllerTestCase
             }
         );
     }
+
+    /**
+     * Ensure that Content-Type and Last-Modified headers can be set from the
+     * model.
+     *
+     * @return void
+     */
+    public function testSetHeaders() {
+        $req = $this->req();
+        $res = $this->res();
+
+        $model = new TestHeadersModel();
+        $controller = new TestHeadersController($req, $res, $model);
+        $controller->get();
+
+        $content_type = $res->headers()->get('Content-Type');
+        $last_modified = $res->headers()->get('Last-Modified');
+
+        $this->assertEquals($model->mimeType(), $content_type);
+        $this->assertEquals($model->lastModified(), $last_modified);
+    }
+}
+
+/**
+ * Test controller for ControllerTest->testSetHeaders()
+ *
+ * @author Conor McDermottroe <conor@mcdermottroe.com>
+ */
+class TestHeadersController
+extends Controller
+{
+    /**
+     * See Controller::__construct
+     *
+     * @param \Klein\Request  $request  The Klein request object.
+     * @param \Klein\Response $response The Klein response object.
+     * @param Model           $model    The model to use. (Should be an 
+     *                                  instance of TestHeadersModel.)
+     */
+    public function __construct($request, $response, $model) {
+        $this->action = 'error';
+        $this->model = $model;
+        parent::__construct($request, $response);
+    }
+}
+
+/**
+ * Test model for ControllerTest->testSetHeaders()
+ *
+ * @author Conor McDermottroe <conor@mcdermottroe.com>
+ */
+class TestHeadersModel
+extends Model
+{
+    /**
+     * A fixed dummy MIME type.
+     *
+     * @return string A dummy MIME type.
+     */
+    public function mimeType() {
+        return 'dummy/mime+type';
+    }
+
+    /**
+     * A Last-Modified header value.
+     *
+     * @return string A fixed, dummy Last-Modified header value.
+     */
+    public function lastModified() {
+        return 'Sun, 07 Sep 2014 08:31:54 +0100';
+    }
 }
 
 ?>
